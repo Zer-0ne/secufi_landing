617f288176c5e96ace8535a337563135fe7511ba
shobhit saini <dev@secufi.in>
2025-12-06 17:25:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
-import Hero from './components/Hero';
-import Features from './components/Features';
-import HowItWorks from './components/HowItWorks';
-import ForAdvisors from './components/ForAdvisors';
-import CTA from './components/CTA';
-import {Routes, Route} from 'react-router-dom'
-import {useState} from 'react'
-import Form from './components/Form.tsx'
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import ForAdvisors from "./components/ForAdvisors";
+import CTA from "./components/CTA";
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Form from "./components/Form.tsx";
 function App() {
   return (
-
-  <Routes>
-    <Route path='/' element={ <div className="min-h-screen bg-[#0a0a0f] text-white overflow-hidden">
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div className="min-h-screen bg-[#0a0a0f] text-white overflow-hidden">
             <Hero />
             <Features />
             <HowItWorks />
-            <ForAdvisors /> 
+            <ForAdvisors />
             <CTA />
-          </div>}/>
+          </div>
+        }
+      />
 
-    <Route path="/contact" element={<Form/>}/>
-  </Routes>
-
-  )
+      <Route path="/contact" element={<Form />} />
+    </Routes>
+  );
 }
 
 export default App;
-
-
-
-
